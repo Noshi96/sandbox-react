2bf8835de2e3588a2d832a2bddac009d470fb37f
Noshi96 <2spawel2@gmail.com>
2023-02-16 20:55:56 +0100

    Add form

--- src/pages/PageTwo.js
+++ src/pages/PageTwo.js
@@ -1,174 +1,151 @@
+import { useState } from 'react';
+import './PageTwo.css';
+
 const PageTwo = () => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const sentence = 'Helo hwo its work man man';
+
+  const vowelsCount = sentence.split('').reduce((acc, letter) => {
+    return Number((acc += vowels.includes(letter) ? 1 : 0));
+  }, 0);
+  const [countVowels] = useState(vowelsCount);
+
+  const INITIAL_STATE = {
+    email: '',
+    password: '',
+  };
+
+  const VALIDATION = {
+    email: [
+      {
+        isValid: (value) => !!value,
+        message: 'Is required.',
+      },
+      {
+        isValid: (value) => /\S+@\S+\.\S+/.test(value),
+        message: 'Needs to be an email.',
+      },
+    ],
+    password: [
+      {
+        isValid: (value) => !!value,
+        message: 'Is required.',
+      },
+    ],
+  };
+
+  const getErrorFields = (form) =>
+    Object.keys(form).reduce((acc, key) => {
+      if (!VALIDATION[key]) return acc;
+
+      const errorsPerField = VALIDATION[key]
+        // get a list of potential errors for each field
+        // by running through all the checks
+        .map((validation) => ({
+          isValid: validation.isValid(form[key]),
+          message: validation.message,
+        }))
+        // only keep the errors
+        .filter((errorPerField) => !errorPerField.isValid);
+
+      return { ...acc, [key]: errorsPerField };
+    }, {});
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const errorFields = getErrorFields(form);
+    console.log(errorFields);
+    const hasErrors = Object.values(errorFields).flat().length > 0;
+    console.log(Object.values(errorFields).flat().length);
+    console.log(errorFields);
+    if (hasErrors) {
+      console.log(errorFields);
+      return;
+    }
+    alert(form.email + ' ' + form.password);
+  };
+
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+
+  const changeEmailHandler = (event) => {
+    setEmail(event.target.value);
+    console.log(password);
+  };
+  const changePasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const [favorite, setFavorite] = useState('dog');
+
+  const handleCatChange = () => {
+    setFavorite('cat');
+  };
+
+  const handleDogChange = () => {
+    setFavorite('dog');
+  };
+
+  const [count, setCount] = useState(0);
+
+  const countHandler = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <>
       <h1>PageTwo</h1>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam
-        nisl elit, non finibus nibh varius quis. Suspendisse sodales ex id mi
-        interdum sollicitudin. Etiam vel venenatis nulla. Integer rhoncus nibh
-        ac pretium pharetra. Praesent maximus sem vel vulputate volutpat. Etiam
-        hendrerit eu lacus eu vestibulum. Mauris vitae magna at velit congue
-        vulputate tristique nec nisi. Morbi et diam nibh. Phasellus mollis,
-        massa et volutpat dapibus, metus metus efficitur risus, non efficitur
-        lacus justo et justo. Vivamus sed malesuada ligula, quis varius sapien.
-        Maecenas porta lectus quis tincidunt luctus. Sed at tellus ipsum. Duis
-        placerat tincidunt metus, a tempor mi mollis eget. Quisque molestie
-        ipsum vel neque viverra, quis tristique justo egestas. Aenean quis
-        pellentesque urna, et ullamcorper risus. Sed vitae purus posuere,
-        tincidunt libero vel, commodo nunc. Sed facilisis velit et cursus
-        sodales. Nunc vulputate, lacus eget ultrices condimentum, quam ipsum
-        scelerisque arcu, nec faucibus quam ipsum in ex. Donec lobortis erat non
-        rutrum faucibus. Donec nec dolor est. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Suspendisse potenti. Morbi neque arcu,
-        varius ut lacus non, pretium malesuada sem. Suspendisse auctor ante a mi
-        porta laoreet. Aenean eu sem quis lectus mattis rhoncus. Sed tincidunt
-        turpis nec est maximus viverra. Aliquam quis felis id lectus ultricies
-        fringilla pulvinar sit amet magna. Pellentesque placerat dignissim odio,
-        vel rutrum dui efficitur suscipit. Donec ultricies ultricies ex nec
-        maximus. Etiam et blandit justo. Duis tempus lectus ut nisl imperdiet
-        fringilla. Cras vitae dui bibendum, ornare velit eget, aliquet tellus.
-        Praesent vitae iaculis purus, et pharetra lorem. Curabitur suscipit
-        luctus auctor. Fusce nisi ipsum, pulvinar sed dignissim quis, efficitur
-        at ligula. Etiam maximus facilisis lorem id commodo. In dolor neque,
-        pulvinar id lorem eleifend, tincidunt tempus turpis. Class aptent taciti
-        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-        Etiam vehicula tortor sem, eu vulputate nisl sodales non. Nullam
-        ultricies libero at nibh efficitur, vitae imperdiet velit auctor. In hac
-        habitasse platea dictumst. Nam elementum magna eu nibh viverra interdum.
-        Vestibulum neque orci, dignissim nec sapien eget, fermentum sollicitudin
-        turpis. Nullam eleifend nec nisl quis pellentesque. Integer eu dolor in
-        metus mattis ullamcorper. Donec venenatis quam vitae dolor tristique
-        semper. Nullam convallis urna sit amet sem efficitur, quis pharetra
-        mauris accumsan. Aliquam molestie tortor nec accumsan interdum. Nullam
-        quis ipsum finibus, maximus ligula eget, tempor eros. Maecenas sed
-        semper neque. Nam eget est quam. Fusce efficitur mauris quis suscipit
-        eleifend. Nulla volutpat interdum feugiat. Suspendisse potenti. Quisque
-        convallis maximus ultricies. Nam ac velit non sapien dignissim eleifend
-        consequat non velit. Maecenas lobortis ipsum a lorem facilisis
-        porttitor. Vestibulum a odio congue enim maximus faucibus. Donec
-        pulvinar ante vel nisl sagittis commodo. Maecenas dignissim, ante sit
-        amet ultricies condimentum, orci dolor suscipit ipsum, in accumsan
-        libero quam vitae augue. Suspendisse potenti. Duis blandit maximus
-        fringilla. Praesent molestie iaculis condimentum. Morbi vel libero sit
-        amet ex blandit porttitor. Proin ac ipsum nec eros eleifend dapibus.
-        Curabitur laoreet leo magna, lacinia aliquet sapien facilisis aliquam.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos. Aliquam erat volutpat. Pellentesque consequat
-        pretium molestie. Integer malesuada diam id nibh ornare, ut ultricies
-        dolor faucibus. Sed vestibulum ex risus, ac facilisis leo pulvinar in.
-        Nunc sagittis nisl at nunc elementum, vel viverra orci pretium. Sed
-        lacus ligula, fermentum sit amet commodo vitae, tincidunt vel magna.
-        Vivamus pulvinar vehicula quam at consectetur. Etiam quis turpis quis
-        augue rutrum luctus luctus et magna. Cras condimentum iaculis ligula non
-        rhoncus. Sed consequat, mauris sed ornare ornare, ex magna egestas
-        tortor, ac euismod sem purus id erat. Etiam id tellus eu dui pulvinar
-        malesuada sit amet sit amet nulla. Nam semper semper sagittis. Maecenas
-        vel tortor maximus, laoreet purus vel, vulputate eros. In at luctus
-        dolor, eget blandit libero. Vivamus id velit pellentesque massa
-        dignissim mollis a non quam. Proin sed mi sed erat luctus semper. Nam id
-        lorem a est aliquet ullamcorper vitae sed nisi. Maecenas quis mi justo.
-        Nam eget sapien eget urna gravida semper. Sed posuere ante in pulvinar
-        gravida. Maecenas sollicitudin, felis quis tristique sagittis, sem justo
-        laoreet ex, ac faucibus ex eros eget ante. Ut eu aliquet purus. Nullam
-        ultricies nec neque vitae pulvinar. Nullam elit sem, rutrum eget semper
-        ac, tincidunt at dui. Integer fermentum malesuada nibh, vel pulvinar dui
-        pharetra a. Quisque eu nulla finibus massa sollicitudin venenatis.
-        Vestibulum ac velit est. Vivamus ut mauris in purus volutpat venenatis.
-        Donec gravida quis nibh scelerisque aliquet. Praesent porttitor, leo ac
-        mollis convallis, arcu nisi malesuada tellus, ut tempus erat diam sed
-        ante. Aliquam felis sapien, elementum non tellus ut, tristique bibendum
-        dolor. Mauris tellus nisl, tincidunt non scelerisque id, condimentum at
-        massa. Vivamus aliquam tincidunt ultrices. Quisque mattis turpis ipsum,
-        non tincidunt dui volutpat ut. Quisque arcu libero, fringilla eget
-        commodo non, vestibulum vel eros. Proin lobortis magna id nisl consequat
-        placerat. Curabitur leo lacus, maximus id sollicitudin ut, scelerisque
-        ac eros. Pellentesque porttitor luctus nisl, ac ultrices sapien sagittis
-        id. Etiam fringilla, nulla sed interdum ultrices, sem est pellentesque
-        ligula, at cursus mi enim sit amet metus. Curabitur eget malesuada enim.
-        Nullam dui sapien, iaculis non condimentum eu, semper in elit. Phasellus
-        dictum purus non ligula tempor, sed sollicitudin felis finibus. Duis
-        ipsum neque, volutpat eu sem quis, semper elementum neque. Etiam eget
-        lorem quis mauris feugiat tincidunt eu sit amet dolor. Fusce ut lorem
-        leo. Sed gravida et nunc vel sollicitudin. Mauris sed feugiat dui. Nulla
-        ex tortor, tristique eget dolor non, scelerisque bibendum leo. Quisque
-        quis dui metus. Integer nibh ex, placerat in faucibus id, molestie
-        pulvinar odio. Etiam auctor ornare libero. Sed est libero, vestibulum
-        nec congue eget, feugiat sit amet massa. Suspendisse id urna nec diam
-        faucibus fermentum a ut metus. Proin orci nisi, pulvinar vel rhoncus ut,
-        vestibulum eget lectus. Cras eu dictum velit, id fermentum felis. Nunc
-        blandit, ante ac lobortis lacinia, ante dolor hendrerit nulla, vel porta
-        massa augue vitae massa. Aliquam mi elit, viverra sed urna non,
-        ullamcorper feugiat justo. Etiam faucibus erat erat, gravida blandit
-        justo tempor nec. Ut quam tellus, iaculis et elit eu, venenatis blandit
-        tellus. Aenean pellentesque, sem nec commodo luctus, diam ipsum
-        tincidunt libero, condimentum tristique ex ipsum rutrum dolor. Nulla
-        ullamcorper fermentum orci, quis accumsan augue. Suspendisse non enim
-        sapien. Quisque efficitur erat vitae est pharetra, quis molestie dolor
-        porta. Sed vel vehicula nibh. Maecenas congue leo tempus, ultrices metus
-        at, tempus leo. Mauris in diam vitae arcu consectetur tempor.
-        Suspendisse faucibus euismod dui vitae rutrum. Mauris eu quam et est
-        fermentum tincidunt et porttitor nisl. Cras accumsan sem turpis, vel
-        porttitor erat vehicula aliquet. Pellentesque faucibus turpis justo, a
-        feugiat libero efficitur vitae. Donec semper venenatis risus sit amet
-        commodo. Donec vel felis augue. Sed rutrum risus et gravida tincidunt.
-        Integer ligula leo, hendrerit a finibus nec, viverra eget turpis. Sed
-        egestas sodales ligula sit amet laoreet. Morbi pulvinar eros non
-        dignissim pulvinar. Aenean eget posuere risus, sit amet vulputate augue.
-        Praesent eleifend tortor vel mauris commodo sagittis. Fusce id diam
-        bibendum, consequat lacus a, maximus ex. Curabitur dapibus rutrum dolor,
-        at convallis orci consectetur laoreet. Fusce vel metus a risus tristique
-        volutpat vitae et velit. Phasellus ultrices mi quis arcu convallis, sed
-        hendrerit massa aliquet. Nam et ipsum ac libero congue elementum
-        bibendum non erat. Phasellus nunc nisl, bibendum sed sollicitudin vitae,
-        molestie in elit. Cras luctus nisi sit amet orci dignissim, a mollis
-        ante dapibus. Aliquam vitae commodo augue. In urna odio, porttitor vitae
-        sodales vel, pellentesque vitae lectus. Pellentesque habitant morbi
-        tristique senectus et netus et malesuada fames ac turpis egestas. In
-        tempus arcu enim, ac maximus augue tincidunt tristique. Nullam at semper
-        est, vel gravida sem. Mauris est turpis, maximus a luctus id, bibendum
-        vitae nisi. Phasellus quis dictum est. Vestibulum ante ipsum primis in
-        faucibus orci luctus et ultrices posuere cubilia curae; Donec tempor
-        aliquam lorem non rutrum. Fusce vitae augue ac mauris pharetra fringilla
-        quis at nulla. Duis at libero convallis diam hendrerit hendrerit ut
-        vitae tellus. Cras tristique nibh non vestibulum laoreet. Quisque eu
-        justo elementum, facilisis quam vitae, malesuada elit. Phasellus ut sem
-        erat. Mauris pellentesque arcu vitae felis sagittis, fermentum tincidunt
-        sapien rutrum. Aliquam eleifend, neque non iaculis iaculis, felis ante
-        aliquam dolor, at lacinia mi erat nec magna. Vivamus lobortis sem at
-        bibendum finibus. Curabitur nec felis fringilla est porttitor cursus.
-        Integer ut elementum nisl. Vivamus ut urna imperdiet, egestas lacus
-        eget, ultricies lacus. Maecenas aliquet, nunc nec tincidunt auctor, arcu
-        sem aliquam ligula, faucibus iaculis ipsum odio eu nulla. Maecenas
-        sollicitudin diam arcu, non tincidunt nulla mattis auctor. Suspendisse
-        convallis lacus sit amet nisi ornare, eu cursus ex rutrum. Duis odio
-        libero, euismod consectetur venenatis quis, blandit vel felis. Curabitur
-        molestie mollis convallis. Morbi a iaculis turpis, eu aliquet leo. Donec
-        quis dictum arcu. Aenean et tincidunt ipsum. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Fusce vel egestas ipsum. Cras rutrum
-        consequat accumsan. Aliquam erat volutpat. Nulla auctor enim id faucibus
-        elementum. Donec elementum, tellus vel dictum lobortis, neque dolor
-        dignissim orci, non rhoncus orci nibh eu nibh. Aenean nec turpis et nisi
-        dapibus consectetur. Sed efficitur ante ac felis dapibus iaculis
-        pellentesque sed nulla. Maecenas scelerisque sagittis arcu, a gravida
-        felis vestibulum vehicula. Praesent volutpat velit sed condimentum
-        imperdiet. Duis posuere ipsum leo, non convallis odio sagittis ac.
-        Curabitur laoreet ipsum eget elit ultricies congue. Vestibulum interdum
-        facilisis laoreet. Praesent bibendum euismod neque eget pharetra.
-        Maecenas id iaculis nisi. Integer condimentum, lacus a consectetur
-        gravida, diam lectus scelerisque nunc, ac mollis libero enim at lectus.
-        Sed scelerisque tempor orci quis finibus. Etiam mauris diam,
-        sollicitudin ut nibh tempus, luctus finibus lorem. Donec nibh nibh,
-        tincidunt quis aliquam quis, mattis eu tellus. Ut tristique a arcu id
-        maximus. Nunc in ornare est. Fusce gravida neque ut sagittis rutrum.
-        Maecenas luctus mi id placerat condimentum. Nunc venenatis congue purus
-        nec dignissim. Phasellus sed dapibus justo, quis dignissim sem. Cras
-        sagittis luctus dui, ut condimentum neque. Aliquam libero neque, viverra
-        ut tempor vel, tempor at felis. Cras leo leo, scelerisque pulvinar
-        tellus quis, pulvinar pulvinar turpis. Maecenas suscipit auctor
-        pharetra. Integer imperdiet ex ac vehicula ultricies. Praesent
-        vestibulum odio dignissim leo facilisis, quis vehicula dolor ultrices.
-      </div>
+      <form onSubmit={handleSubmit} className='my-form'>
+        <label htmlFor='cheese'>Do you like cheese?</label>
+        <input type='checkbox' name='cheese' id='cheese' />
+        <label htmlFor='email'>Email</label>
+        <input type='text' id='email' onChange={handleChange} />
+        <label htmlFor='password'>Pass Password</label>
+        <input type={'password'} id='password' onChange={handleChange} />
+        <RadioButton
+          label='Cat'
+          value={favorite === 'cat'}
+          onChange={handleCatChange}
+        />
+        <RadioButton
+          label='Dog'
+          value={favorite === 'dog'}
+          onChange={handleDogChange}
+        />
+        <button type='submit'>Send</button>
+      </form>
+      {/* {finalData.map((element) => {
+        return <p>{element.name}</p>
+      })} */}
+      <h1>{form.password}</h1>
+      <br></br>
+      <button onClick={countHandler}>Count</button>
+      {count}
+      {countVowels}
     </>
-  )
-}
-export default PageTwo
+  );
+};
+
+const RadioButton = ({ label, value, onChange }) => {
+  return (
+    <label>
+      <input type='radio' checked={value} onChange={onChange} />
+      {label}
+    </label>
+  );
+};
+
+export default PageTwo;
